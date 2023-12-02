42087d7e9e2205a94d8073835f6c91059c34c08e
OASEN <71011762+A233S@users.noreply.github.com>
2023-12-03 00:24:14 +0800

    Update channel.go

--- model/channel.go
+++ model/channel.go
@@ -11,24 +11,24 @@ type Channel struct {
     Key                      string  `json:"key" gorm:"not null;index"`
     Status                   int     `json:"status" gorm:"default:1"`
     Name                     string  `json:"name" gorm:"index"`
-    Weight                   *uint   `json:"weight" gorm:"default:0"`  
+    Weight                   *uint   `json:"weight" gorm:"default:0"`
     CreatedTime              int64   `json:"created_time" gorm:"bigint"`
     TestTime                 int64   `json:"test_time" gorm:"bigint"`
     ResponseTime             int     `json:"response_time"` // in milliseconds
     FullURL                  string  `json:"full_url" gorm:"column:full_url;default:''"`
-    BaseURL                  *string `json:"base_url" gorm:"column:base_url;default:''"`    
+    BaseURL                  *string `json:"base_url" gorm:"column:base_url;default:''"`
     Other                    string  `json:"other"`
     Balance                  float64 `json:"balance"` // in USD
     BalanceUpdatedTime       int64   `json:"balance_updated_time" gorm:"bigint"`
     Models                   string  `json:"models"`
     Group                    string  `json:"group" gorm:"type:varchar(32);default:'default'"`
     UsedQuota                int64   `json:"used_quota" gorm:"bigint;default:0"`
+    AsyncNum                 int     `json:"async_num" gorm:"column:async_num;default:1"`
     ModelMapping             *string `json:"model_mapping" gorm:"type:varchar(1024);default:''"`
+    Priority                 *int64  `json:"priority" gorm:"bigint;default:0"`
     Sort                     *int    `json:"sort"`
     OverFrequencyAutoDisable *bool   `json:"overFrequencyAutoDisable" gorm:"default:0"`
     RetryInterval            *int    `json:"retryInterval" gorm:"default:300"`
-    AsyncNum                 int     `json:"async_num" gorm:"column:async_num;default:1"`
-    Priority                 *int64  `json:"priority" gorm:"bigint;default:0"`
 }
 
 func GetAllChannels(startIdx int, num int, selectAll bool) ([]*Channel, error) {
@@ -88,11 +88,33 @@ func BatchInsertChannels(channels []Channel) error {
     return nil
 }
 
+func (channel *Channel) GetPriority() int64 {
+    if channel.Priority == nil {
+        return 0
+    }
+    return *channel.Priority
+}
+
+func (channel *Channel) GetBaseURL() string {
+    if channel.BaseURL == nil {
+        return ""
+    }
+    return *channel.BaseURL
+}
+
+func (channel *Channel) GetModelMapping() string {
+    if channel.ModelMapping == nil {
+        return ""
+    }
+    return *channel.ModelMapping
+}
+
 func (channel *Channel) Insert() error {
     var err error
     err = DB.Create(channel).Error
     if err != nil {
         return err
+    
     }
     err = channel.AddAbilities()
     return err
@@ -151,14 +173,6 @@ func UpdateChannelStatusById(id int, status int) {
 }
 
 func UpdateChannelUsedQuota(id int, quota int) {
-    if common.BatchUpdateEnabled {
-        addNewRecord(BatchUpdateTypeChannelUsedQuota, id, quota)
-        return
-    }
-    updateChannelUsedQuota(id, quota)
-}
-
-func updateChannelUsedQuota(id int, quota int) {
     err := DB.Model(&Channel{}).Where("id = ?", id).Update("used_quota", gorm.Expr("used_quota + ?", quota)).Error
     if err != nil {
         common.SysError("failed to update channel used quota: " + err.Error())
