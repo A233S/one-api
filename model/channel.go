68fd38c23306ac9b45b756bd8661f986fec3f2e6
OASEN <71011762+A233S@users.noreply.github.com>
2023-12-03 01:02:14 +0800

    Update channel.go

--- model/channel.go
+++ model/channel.go
@@ -6,31 +6,33 @@ import (
 )
 
 type Channel struct {
-    Id                       int      `json:"id"`
-    Type                     int      `json:"type" gorm:"default:0"`
-    Key                      string   `json:"key" gorm:"not null;index"`
-    Status                   int      `json:"status" gorm:"default:1"`
-    Name                     string   `json:"name" gorm:"index"`
-    Weight                   int      `json:"weight"`
-    CreatedTime              int64    `json:"created_time" gorm:"bigint"`
-    TestTime                 int64    `json:"test_time" gorm:"bigint"`
-    ResponseTime             int      `json:"response_time"` // in milliseconds
-    BaseURL                  string   `json:"base_url" gorm:"column:base_url"`
-    Other                    string   `json:"other"`
-    Balance                  float64  `json:"balance"` // in USD
-    BalanceUpdatedTime       int64    `json:"balance_updated_time" gorm:"bigint"`
-    Models                   string   `json:"models"`
-    Group                    string   `json:"group" gorm:"type:varchar(32);default:'default'"`
-    UsedQuota                int64    `json:"used_quota" gorm:"bigint;default:0"`
-    ModelMapping             string   `json:"model_mapping" gorm:"type:varchar(1024);default:''"`
-    Sort                     *int     `json:"sort"`
-    OverFrequencyAutoDisable *bool    `json:"overFrequencyAutoDisable" gorm:"default:0"`
-    RetryInterval            *int     `json:"retryInterval" gorm:"default:300"`
-    FullURL                  string   `json:"full_url" gorm:"column:full_url;default:''"`
-    BaseURLType              *string  `json:"base_url" gorm:"column:base_url;default:''"`
-    ModelMappingType         *string  `json:"model_mapping" gorm:"type:varchar(1024);default:''"`
-    Priority                 *int64   `json:"priority" gorm:"bigint;default:0"`
+    Id                       int     `json:"id"`
+    Type                     int     `json:"type" gorm:"default:0"`
+    Key                      string  `json:"key" gorm:"not null;index"`
+    Status                   int     `json:"status" gorm:"default:1"`
+    Name                     string  `json:"name" gorm:"index"`
+    Weight                   int     `json:"weight"`
+    CreatedTime              int64   `json:"created_time" gorm:"bigint"`
+    TestTime                 int64   `json:"test_time" gorm:"bigint"`
+    ResponseTime             int     `json:"response_time"` // in milliseconds
+    BaseURL                  string  `json:"base_url" gorm:"column:base_url"`
+    Other                    string  `json:"other"`
+    Balance                  float64 `json:"balance"` // in USD
+    BalanceUpdatedTime       int64   `json:"balance_updated_time" gorm:"bigint"`
+    Models                   string  `json:"models"`
+    Group                    string  `json:"group" gorm:"type:varchar(32);default:'default'"`
+    UsedQuota                int64   `json:"used_quota" gorm:"bigint;default:0"`
+    ModelMapping             string  `json:"model_mapping" gorm:"type:varchar(1024);default:''"`
+    Sort                     *int    `json:"sort"`
+    OverFrequencyAutoDisable *bool   `json:"overFrequencyAutoDisable" gorm:"default:0"`
+    RetryInterval            *int    `json:"retryInterval" gorm:"default:300"`
+    FullURL                  string  `json:"full_url" gorm:"column:full_url;default:''"`
+    BaseURLType              string  `json:"base_url" gorm:"column:base_url;default:''"`
+    ModelMappingType         string  `json:"model_mapping" gorm:"type:varchar(1024);default:''"`
+    Priority                 *int64  `json:"priority" gorm:"bigint;default:0"`
 }
+
+
 
 func GetAllChannels(startIdx int, num int, selectAll bool) ([]*Channel, error) {
     var channels []*Channel
@@ -97,17 +99,11 @@ func (channel *Channel) GetPriority() int64 {
 }
 
 func (channel *Channel) GetBaseURL() string {
-    if channel.BaseURL == nil {
-        return ""
-    }
-    return *channel.BaseURL
+    return channel.BaseURL
 }
 
 func (channel *Channel) GetModelMapping() string {
-    if channel.ModelMapping == nil {
-        return ""
-    }
-    return *channel.ModelMapping
+    return channel.ModelMapping
 }
 
 func (channel *Channel) Insert() error {
