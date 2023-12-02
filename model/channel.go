b2a29fa416b1c9a95163649419129079c4ac5190
OASEN <71011762+A233S@users.noreply.github.com>
2023-12-03 01:18:53 +0800

    Update channel.go

--- model/channel.go
+++ model/channel.go
@@ -6,25 +6,25 @@ import (
 )
 
 type Channel struct {
-	Id                       int     `json:"id"`
-	Type                     int     `json:"type" gorm:"default:0"`
-	Key                      string  `json:"key" gorm:"not null;index"`
-	Status                   int     `json:"status" gorm:"default:1"`
-	Name                     string  `json:"name" gorm:"index"`
-	Weight                   int     `json:"weight"`
-	CreatedTime              int64   `json:"created_time" gorm:"bigint"`
-	TestTime                 int64   `json:"test_time" gorm:"bigint"`
-	ResponseTime             int     `json:"response_time"` // in milliseconds
+	Id                 int     `json:"id"`
+	Type               int     `json:"type" gorm:"default:0"`
+	Key                string  `json:"key" gorm:"not null;index"`
+	Status             int     `json:"status" gorm:"default:1"`
+	Name               string  `json:"name" gorm:"index"`
+	Weight             *uint   `json:"weight" gorm:"default:0"`
+	CreatedTime        int64   `json:"created_time" gorm:"bigint"`
+	TestTime           int64   `json:"test_time" gorm:"bigint"`
+	ResponseTime       int     `json:"response_time"` // in milliseconds
 	FullURL            string  `json:"full_url" gorm:"column:full_url;default:''"`
-	BaseURL                  string  `json:"base_url" gorm:"column:base_url"`
-	Other                    string  `json:"other"`
-	Balance                  float64 `json:"balance"` // in USD
-	BalanceUpdatedTime       int64   `json:"balance_updated_time" gorm:"bigint"`
-	Models                   string  `json:"models"`
-	Group                    string  `json:"group" gorm:"type:varchar(32);default:'default'"`
-	UsedQuota                int64   `json:"used_quota" gorm:"bigint;default:0"`
+	BaseURL            *string `json:"base_url" gorm:"column:base_url;default:''"`
+	Other              string  `json:"other"`
+	Balance            float64 `json:"balance"` // in USD
+	BalanceUpdatedTime int64   `json:"balance_updated_time" gorm:"bigint"`
+	Models             string  `json:"models"`
+	Group              string  `json:"group" gorm:"type:varchar(32);default:'default'"`
+	UsedQuota          int64   `json:"used_quota" gorm:"bigint;default:0"`
 	AsyncNum           int     `json:"async_num" gorm:"column:async_num;default:1"`
-	ModelMapping             string  `json:"model_mapping" gorm:"type:varchar(1024);default:''"`
+	ModelMapping       *string `json:"model_mapping" gorm:"type:varchar(1024);default:''"`
 	Sort                     *int    `json:"sort"`
 	OverFrequencyAutoDisable *bool   `json:"overFrequencyAutoDisable" gorm:"default:0"`
 	Priority           *int64  `json:"priority" gorm:"bigint;default:0"`
